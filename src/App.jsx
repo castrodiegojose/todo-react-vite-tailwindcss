14755db39bb581f60bf5bfbda9e080b97ab84d16
Diego <castrodiegojose@gmail.com>
2023-09-10 18:46:00 -0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import TodoCreate from "./components/TodoCreate";
 import TodoList from "./components/TodoList";
 import TodoComputed from "./components/TodoComputed";
 import TodoFilter from "./components/TodoFilter";
 
-const initialStateTodos = [
-  { id: 1, title: "Go to the gym", completed: true },
-  { id: 2, title: "Do homework", completed: false },
-  { id: 3, title: "shopping", completed: false },
-  { id: 4, title: "Meditation", completed: false },
-  { id: 5, title: "Complete Course", completed: false },
-];
+// const initialStateTodos = [
+//   { id: 1, title: "Go to the gym", completed: true },
+//   { id: 2, title: "Do homework", completed: false },
+//   { id: 3, title: "shopping", completed: false },
+//   { id: 4, title: "Meditation", completed: false },
+//   { id: 5, title: "Complete Course", completed: false },
+// ];
+
+const initialStateTodos = JSON.parse(localStorage.getItem("todos")) || [];
 
 function App() {
   const [todos, setTodos] = useState(initialStateTodos);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const createTodo = (title) => {
     const newTodo = {
@@ -23,7 +29,7 @@ function App() {
       completed: false,
     };
 
-    setTodos({ ...todos, newTodo });
+    setTodos([...todos, newTodo]);
   };
 
   const updateTodo = (id) => {
@@ -61,7 +67,7 @@ function App() {
     }
   };
   return (
-    <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain bg-slate-200 min-h-screen dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] transition-all duration-500">
+    <div className="min-h-screen bg-gray-300 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat transition-all duration-500 dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] md:bg-[url('./assets/images/bg-desktop-light.jpg')] md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')]">
       <Header />
 
       <main className="container mx-auto mt-8 px-4">
@@ -78,7 +84,7 @@ function App() {
         <TodoFilter changeFilter={changeFilter} filter={filter} />
       </main>
 
-      <footer className="text-center mt-8 dark:bg-gray-900 dark:text-gray-400">
+      <footer className="text-center mt-8 dark:text-gray-400">
         Drag and drop to reorder list
       </footer>
     </div>
